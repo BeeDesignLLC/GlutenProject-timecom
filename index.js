d70617f06640c77d01e7808b7511ed5f2a496b2b
Brandon Bayer <b@bayer.ws>
2018-03-17 12:26:04 -0400

    new: add triggers for thrive and nuts.com

--- index.js
+++ index.js
@@ -8,5 +8,7 @@ const DATACOM = 'https://datacom.glutenproject.com'
 const TOKEN = '?token=' + process.env.TGP_NETWORK_TOKEN
 
 raven.context(() => {
-  scheduleJob({minute: 0}, () => fetch(DATACOM + '/amazon' + TOKEN) && console.log(new Date(), 'Triggered Amazon'))
+  scheduleJob({minute: 0}, () => fetch(DATACOM + '/amazon' + TOKEN) && console.log('Triggered Amazon'))
+  scheduleJob({hour: 7, minute: 15}, () => fetch(DATACOM + '/thrive' + TOKEN) && console.log('Triggered Thrive'))
+  scheduleJob({hour: 8, minute: 15, dayOfWeek: 6}, () => fetch(DATACOM + '/nuts' + TOKEN) && console.log('Triggered Nuts'))
 })
