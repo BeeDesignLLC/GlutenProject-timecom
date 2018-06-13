93008e798ed3d8d0f1f56bbd46ce8dbfa001cafc
Brandon Bayer <b@bayer.ws>
2018-06-13 17:32:01 -0400

    new: azure sync

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const TOKEN = '?token=' + process.env.TGP_NETWORK_TOKEN
 
 raven.context(() => {
   scheduleJob({minute: 0}, () => fetch(DATACOM + '/amazon' + TOKEN) && console.log('Triggered Amazon'))
+  scheduleJob({hour: 5, minute: 15}, () => fetch(DATACOM + '/azure' + TOKEN) && console.log('Triggered Azure'))
   scheduleJob({hour: 6, minute: 15}, () => fetch(DATACOM + '/luckyvitamin' + TOKEN) && console.log('Triggered LuckyVitamin'))
   scheduleJob({hour: 7, minute: 15}, () => fetch(DATACOM + '/thrive' + TOKEN) && console.log('Triggered Thrive'))
   scheduleJob({hour: 8, minute: 15, dayOfWeek: 6}, () => fetch(DATACOM + '/nuts' + TOKEN) && console.log('Triggered Nuts'))
