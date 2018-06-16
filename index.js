9e9fddef659580043eacf7a9bd3a79bb23747dec
Brandon Bayer <b@bayer.ws>
2018-06-16 15:35:48 -0400

    new: vitacost sync

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const TOKEN = '?token=' + process.env.TGP_NETWORK_TOKEN
 
 raven.context(() => {
   scheduleJob({minute: 0}, () => fetch(DATACOM + '/amazon' + TOKEN) && console.log('Triggered Amazon'))
+  scheduleJob({hour: 4, minute: 15}, () => fetch(DATACOM + '/vitacost' + TOKEN) && console.log('Triggered Vitacost'))
   scheduleJob({hour: 5, minute: 15}, () => fetch(DATACOM + '/azure' + TOKEN) && console.log('Triggered Azure'))
   scheduleJob({hour: 6, minute: 15}, () => fetch(DATACOM + '/luckyvitamin' + TOKEN) && console.log('Triggered LuckyVitamin'))
   scheduleJob({hour: 7, minute: 15}, () => fetch(DATACOM + '/thrive' + TOKEN) && console.log('Triggered Thrive'))
